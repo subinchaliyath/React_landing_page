7eaeda4e0276340efd64ddfde1b91f21483f725b
Subin Chaliyath <subinchaliyath@gmail.com>
2020-06-08 23:15:04 +0530

    main section addded

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 
 import Header from "./components/header";
+import Main from './components/main';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
